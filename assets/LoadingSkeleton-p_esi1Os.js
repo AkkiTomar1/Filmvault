742ad2ee1891c66ce9e4a945fbde772425672662
AkkiTomar1 <AkkiTomar1@users.noreply.github.com>
2026-09-06 12:20:16 +0000

    deploy: 6d0e708a7feaacc7194428149e520c4c28366636

---
+++ assets/LoadingSkeleton-p_esi1Os.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D2w4eNxT.js";function i({count:s=20}){return r.jsx("div",{className:"grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6","data-testid":"loading-skeleton",children:Array.from({length:s}).map((d,a)=>r.jsx("div",{className:"aspect-[2/3] w-full animate-pulse rounded-xl bg-gray-300","aria-hidden":"true"},a))})}export{i as L};
